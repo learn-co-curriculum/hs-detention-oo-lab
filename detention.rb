e9714150d6a3df6e61b54529a75677c18baa250a
vanessadean <vanessa.dean@gmail.com>
2014-09-11 17:29:18 -0400

    Passes tests

--- detention.rb
+++ detention.rb
@@ -1,1 +1,52 @@
 #Code your detention class here
+class Detention
+
+  def initialize
+    @activity = "collective punishment"
+    @students = []
+  end
+
+  def location=(room_number)
+    @room_number = room_number
+  end
+
+  def location
+    @room_number
+  end
+
+  def time=(time)
+    @time = time
+  end
+
+  def time
+    @time
+  end
+
+  def grumpy_teacher=(name)
+    @name = name
+  end
+
+  def grumpy_teacher
+    @name
+  end
+
+  def activity=(activity)
+    @activity = activity
+  end
+
+  def activity
+    @activity
+  end
+
+  def add_student(student)
+    @students << student
+  end
+
+  def remove_student(student)
+    @students.delete(student)
+  end
+
+  def students
+    @students
+  end  
+end
